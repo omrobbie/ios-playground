e5dd932db2ffd209e68d10ca8042041314ce8ba0
omrobbie <putu.roby@yahoo.com>
2018-05-10 17:44:27 +0800

    tambahkan text dibawah label yang pertama

--- DesignCode-Designn.playground/Contents.swift
+++ DesignCode-Designn.playground/Contents.swift
@@ -13,8 +13,14 @@ class MyViewController: UIViewController {
         label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
         label.text = "Hello World!"
         label.textColor = .black
-        
         view.addSubview(label)
+        
+        let label2 = UILabel()
+        label2.frame = CGRect(x: 150, y: 220, width: 200, height: 20)
+        label2.text = "Learn Swift 4"
+        label2.textColor = .blue
+        view.addSubview(label2)
+
         self.view = view
     }
 }
