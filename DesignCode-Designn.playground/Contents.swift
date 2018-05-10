6782ef029ef2fc62fbace88e874e1a6bc8343e08
omrobbie <putu.roby@yahoo.com>
2018-05-10 17:59:04 +0800

    ubah jenis font dan ukurannya

--- DesignCode-Designn.playground/Contents.swift
+++ DesignCode-Designn.playground/Contents.swift
@@ -10,19 +10,20 @@ class MyViewController: UIViewController {
         view.backgroundColor = .white
         
         let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.frame = CGRect(x: 40, y: 200, width: 200, height: 20)
         label.text = "Hello World!"
         label.textColor = .black
         view.addSubview(label)
         
         let label2 = UILabel()
-        label2.frame = CGRect(x: 150, y: 220, width: 200, height: 20)
+        label2.frame = CGRect(x: 40, y: 220, width: 200, height: 40)
         label2.text = "Learn Swift 4"
         label2.textColor = .blue
+        label2.font = UIFont.systemFont(ofSize: 32, weight: .semibold)
         view.addSubview(label2)
         
         let cardView = UIView()
-        cardView.frame = CGRect(x: 40, y: 255, width: 300, height: 250)
+        cardView.frame = CGRect(x: 40, y: 270, width: 300, height: 250)
         cardView.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
         cardView.layer.cornerRadius = 14
         cardView.layer.shadowOpacity = 0.25
