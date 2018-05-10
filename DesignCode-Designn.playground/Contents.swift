453f9a41f6bdacbdba162f4432fa8fb47f2e0c29
omrobbie <putu.roby@yahoo.com>
2018-05-10 17:43:04 +0800

    design layout dengan kode Swift

--- DesignCode-Designn.playground/Contents.swift
+++ DesignCode-Designn.playground/Contents.swift
@@ -1,5 +1,23 @@
-//: Playground - noun: a place where people can play
+//: # DesignCode - Design
+//: Membuat design layout aplikasi dengan hanya menggunakan kode Swift
 
 import UIKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+class MyViewController: UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hello World!"
+        label.textColor = .black
+        
+        view.addSubview(label)
+        self.view = view
+    }
+}
+
+// ini adalah perintah untuk menampilkan hasil design layout dari class diatas
+PlaygroundPage.current.liveView = MyViewController()
